d25f81ad57505f52a3da889286b7daae0630e745
soulaimane2 <mr.soulaimane.ng@gmail.com>
2025-01-10 05:55:58 +0100

    use the env vars instead of hardcoded values

--- app/core/config.py
+++ app/core/config.py
@@ -1,7 +1,8 @@
 from pydantic_settings import BaseSettings
+import os
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "books tersea coding challenge"
-    DATABASE_URL: str = "mongodb://localhost:27017"
+    DATABASE_URL: str = os.environ["DATABASE_URL"]
 
 settings = Settings()
